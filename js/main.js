9a9a4afb07f78ccde7d7ad42c879953f50ab31ac
Navfront <b3rt@yandex.ru>
2022-03-14 17:49:59 +0300

    hw 1 rdy

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const getRandomArbitrary = (min, max) => {
+  if (min < 0 || max < 0 ) {
+    return -1;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.round(Math.floor(Math.random() * (max - min + 1) + min));
+};
+
+
+
+const isStringLengthOk = (text, maxLength) => {
+  return text.length <= maxLength
+};
+
+
+getRandomArbitrary(20, 40);
+isStringLengthOk('some string', 15);
